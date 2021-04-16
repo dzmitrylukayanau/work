2388f96cb51e78985a0c2677079430cba25cf7f9
root <root@stretch.localdomain>
2021-04-16 22:13:18 +0000

    task_6_2b.py done

--- exercises/06_control_structures/task_6_2b.py
+++ exercises/06_control_structures/task_6_2b.py
@@ -8,3 +8,35 @@
 
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 """
+
+ip = input('Введите IP-адрес в формате x.x.x.x: ')
+ip_correct = False
+
+while not ip_correct:
+    if len(ip.split('.')) != 4:
+        print('Неправильный IP-адрес\n')
+        ip = input('Введите IP-адрес в формате x.x.x.x: ')
+    elif ip.split('.')[0].isdigit() and ip.split('.')[1].isdigit() and ip.split('.')[2].isdigit() and ip.split('.')[3].isdigit():
+        if 255 >= int(ip.split('.')[0]) >= 0 and 255 >= int(ip.split('.')[1]) >= 0 and 255 >= int(ip.split('.')[2]) >= 0 and 255 >= int(ip.split('.')[3]) >= 0:
+            if 0 < int(ip.split('.')[0]) < 224:
+                print('unicast')
+                ip_correct = True
+            elif 224 <= int(ip.split('.')[0]) <= 239:
+                print('multicast')
+                ip_correct = True
+            elif ip == '255.255.255.255':
+                print('local broadcast')
+                ip_correct = True
+            elif ip == '0.0.0.0':
+                print('unassigned')
+                ip_correct = True
+            else:
+                print('unused')
+                ip_correct = True
+        else:
+            print('Неправильный IP-адрес\n')
+            ip = input('Введите IP-адрес в формате x.x.x.x: ')
+    else:
+        print('Неправильный IP-адрес\n')
+        ip = input('Введите IP-адрес в формате x.x.x.x: ')
+
