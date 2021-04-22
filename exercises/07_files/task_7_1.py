f44c007cc39c2b40aaf58884dd91cd69c2ee16f7
root <root@stretch.localdomain>
2021-04-22 19:11:02 +0000

    task_7_1.py done

--- exercises/07_files/task_7_1.py
+++ exercises/07_files/task_7_1.py
@@ -14,3 +14,17 @@ Outbound Interface    FastEthernet0/0
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 
 """
+keys = ['Prefix', 'AD/Metric', 'Next-Hop', 'Last update', 'Outbound Interface'] 
+
+with open('ospf.txt') as f:
+    for line in f:
+        line = line.split()
+        if line:
+            print(f''' 
+                {keys[0]:<22} {line[1]:<22} 
+                {keys[1]:<22} {line[2].strip('[]'):<22} 
+                {keys[2]:<22} {line[4].rstrip(','):<22} 
+                {keys[3]:<22} {line[5].strip(','):<22} 
+                {keys[4]:<22} {line[6]:<22} 
+                ''')                              
+
